c48ff2970d32914b552155fb8a9ca481d5b9fc37
Piellia Vasyl <pellia@ukr.net>
2016-04-01 11:32:03 +0300

    show results on home page

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
@@ -14,5 +14,7 @@ from models import Vacancy, Location
 db.create_all()
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html',
+                           vacancies=Vacancy.query.all()[0:10]
+                           )
